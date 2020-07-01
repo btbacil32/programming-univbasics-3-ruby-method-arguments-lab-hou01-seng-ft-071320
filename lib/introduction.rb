3e34c091c3089e8a300994ba27491875915e6452
bacil thalji <btbacil32@gmail.com>
2020-07-01 07:52:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,16 +1,1 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
-end
-introduction("bizzle")
-
-
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-introduction_with_language("George" , "Ruby")
-
-
-def introduction_with_language_optional(name , language = "Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-introduction_with_language_optional("Mann")
+def introduction
